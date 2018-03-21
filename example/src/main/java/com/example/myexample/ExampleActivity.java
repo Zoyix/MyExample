88dbe7f0e90decaaa0c864c3a2b8189d9c4d2696
Zoyix <Zoyixwork@163.com>
2018-03-21 15:39:40 +0800

    基础版1.1（看说明）

--- example/src/main/java/com/example/myexample/ExampleActivity.java
+++ example/src/main/java/com/example/myexample/ExampleActivity.java
@@ -1,10 +1,30 @@
 package com.example.myexample;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+
 import com.example.latte.core.activities.ProxyActivity;
 import com.example.latte.core.delegates.LatteDelegate;
 
+import qiu.niorgai.StatusBarCompat;
+
 public class ExampleActivity extends ProxyActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //隐藏ActionBar
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        //引入包后（别忘记project的gradle文件中加入maven），一句话让实现状态栏隐藏
+        StatusBarCompat.translucentStatusBar(this, true);
+    }
 
     @Override
     public LatteDelegate setRootDelegate() {
